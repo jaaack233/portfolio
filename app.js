cef7f37ef9de72957f788dc578fe54abc67ae96f
jaaack233 <jackchen990227@gmail.com>
2021-08-24 16:27:35 +0800

    update about

--- app.js
+++ app.js
@@ -17,13 +17,15 @@ var index = require('./routes/index');
 var venmo = require('./routes/venmo');
 var focus = require('./routes/focus');
 var classmate = require('./routes/classmate');
-var mcdonald = require('./routes/mcdonald')
+var mcdonald = require('./routes/mcdonald');
+var about = require('./routes/about');
 
 app.get('/', index.view);
 app.get('/venmo', venmo.view);
 app.get('/focus-co', focus.view);
 app.get('/classmate', classmate.view);
 app.get('/mcdonald', mcdonald.view);
+app.get('/about', about.view);
 
 app.listen(app.get('port'), () => {
     console.log('Express server listening on port ' + app.get('port'));
